69af3d822b0231cabfb895b127cafb9fec8baccf
SzymBurz <szb210@gmail.com>
2023-02-21 12:52:38 +0100

    initial commit

---
+++ src/main/java/com/wtd/eurekaServer/eurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.wtd.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class eurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(eurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtd/eurekaServer/DsadadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtd.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsadadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
